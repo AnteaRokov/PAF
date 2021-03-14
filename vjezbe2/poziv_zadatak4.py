b286f620368e714bc33da420e0fffc3ef1dffeb3
AnteaRokov <arokov@pmfst.hr>
2021-03-15 00:05:03 +0100

    modul u kojem pozivam sve funkcije 4. zadatka

---
+++ vjezbe2/poziv_zadatak4.py
@@ -0,0 +1,6 @@
+import zadatak4
+zadatak4.kraj_gibanja(5, 9.81, 30,3.14 ,10)
+zadatak4.maks_visina(5,3.14,30, 9.81)
+zadatak4.domet(5, 3.14,30, 9.81)
+zadatak4.maks_brzina(5, 30,3.14, 9.81)
+zadatak4.meta_i_hitac(5, 30, 3.14, 10, 8)
