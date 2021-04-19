070b5d979d1e47fee5fcec921e19811af1bdc0ce
AnteaRokov <arokov@pmfst.hr>
2021-04-19 09:15:34 +0200

    bug2

--- vjezbe5/apsolutna_pogreska.py
+++ vjezbe5/apsolutna_pogreska.py
@@ -4,23 +4,6 @@ import numpy as np
 
 h1 = harm.HarmonicOscillator()
 
-
-def preciznost():
-    lista_delta_t = list(np.linspace(0.01,0.11, 50))
-    lista_numericki_period = []
-    analiticki_period = []
-
-    for delta_t in lista_delta_t:
-        h1.init(0.1 ,2 , 0, 0.1, delta_t)    
-        lista_numericki_period.append(h1.period())
-        analiticki_period.append(h1.period_analiticki())
-        h1.reset()
-
-    plt.plot(lista_delta_t, analiticki_period)
-    plt.scatter(lista_delta_t, lista_numericki_period, s = 3, c = "blue")
-    plt.show()
-
-
 def apsolutna_pogreska():
     lista_delta_t = list(np.linspace(0.01 ,0.11 , 50))
     apsolutna_pogreska = []
@@ -34,4 +17,4 @@ def apsolutna_pogreska():
     plt.show()
 
 apsolutna_pogreska()
-preciznost()
+
