ce25121a475758230a7ea88d17703ee9b54302e0
AnteaRokov <arokov@pmfst.hr>
2021-06-04 01:14:08 +0200

    update

---
+++ vjezbe10/zadatak1.py
@@ -0,0 +1,7 @@
+import numpy as np
+import modul as em
+import matplotlib.pyplot as plt
+
+p2 = em.field(0.4,0,0,0,1,np.array([0.1,0.1,0.1]),np.array([0,0,1]),np.array([0,0,0]))
+p2.zad_1()
+p2.reset()
---
+++ vjezbe10/zadatak2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import modul as em
+import matplotlib.pyplot as plt
+
+p2 = em.field(0.4,0,0,0,1,np.array([0.1,0.1,0.1]),np.array([0,0,1]),np.array([0,0,0]))
+p2.zad_2()
+p2.reset()
